cfb871991c23c7b5f8d8eb10bf9bd62fd0de5b3f
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-14 13:06:57 +0100

    Create NotFound.vue

---
+++ src/components/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>ERROR 404</h1>
+    <h1>PAGE NOT FOUND</h1>
+  </div>
+  <div>CLICK  <span style="font-style: italic;"><RouterLink to="/">here</RouterLink></span> TO GO HOME</div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped>
+h1 {
+  display: block;
+  font-size: 4rem;
+}
+div {
+  color: white;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  font-size: 1.6rem;
+  margin-top: 4rem;
+}
+</style>
