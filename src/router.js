38e707b9a14d17dd9fbc618da1b60acee9c7a78b
zacheusIsrael <segunzacheusi@gmail.com>
2023-03-14 13:07:14 +0100

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from './components/HomePage.vue'
+import CounterApp from "./components/CounterApp.vue";
+import NotFound from "./components/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/counter", component: CounterApp },
+    { path: "/:pathMatch(.*)*", component: NotFound },
+  ],
+});
+
+export default router;
